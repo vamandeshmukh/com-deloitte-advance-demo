740a29528ff032636c976c393c1a7dbb5f040621
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-07-19 10:57:26 +0530

    coreJava

---
+++ src/main/java/com/deloitte/advance/demo/JdbcDemo.java
@@ -0,0 +1,13 @@
+package com.deloitte.advance.demo;
+
+public class JdbcDemo {
+
+	public static void main(String[] args) {
+
+		System.out.println("Start");
+
+		System.out.println("End");
+
+	}
+
+}
