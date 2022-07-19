75bb7aa449a5f76c38f6a86b26459a4f845f631c
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-07-19 12:11:56 +0530

    coreJava

--- src/main/java/com/deloitte/advance/demo/dao/StudentDao.java
+++ src/main/java/com/deloitte/advance/demo/dao/StudentDao.java
@@ -1,8 +1,21 @@
 package com.deloitte.advance.demo.dao;
 
-public interface StudentDao {
-	
+import java.util.List;
 
-	// CRUD ops 
+import com.deloitte.advance.demo.model.Student;
+
+public interface StudentDao {
+
+	// CRUD ops
+
+	public abstract Student addStudent(Student student);
+
+	public abstract Student updateStudent(Student student);
+
+	public abstract Student deleteStudentByRollNo(int rollNo);
+
+	public abstract Student getStudentByRollNo(int rollNo);
+
+	public abstract List<Student> getAllStudents();
 
 }
--- src/main/java/com/deloitte/advance/demo/dao/StudentDaoImpl.java
+++ src/main/java/com/deloitte/advance/demo/dao/StudentDaoImpl.java
@@ -1,5 +1,39 @@
 package com.deloitte.advance.demo.dao;
 
-public class StudentDaoImpl {
+import java.util.List;
+
+import com.deloitte.advance.demo.model.Student;
+
+public class StudentDaoImpl implements StudentDao {
+
+	@Override
+	public Student addStudent(Student student) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student updateStudent(Student student) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student deleteStudentByRollNo(int rollNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student getStudentByRollNo(int rollNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Student> getAllStudents() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
