d4b90d38f2c23cf5d24f3c0ca66dde0d9a114536
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-07-19 11:52:32 +0530

    coreJava

--- src/main/java/com/deloitte/advance/demo/JdbcDemo2.java
+++ src/main/java/com/deloitte/advance/demo/JdbcDemo2.java
@@ -7,62 +7,26 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class JdbcDemo {
+public class JdbcDemo2 {
 
 	public static void main(String[] args) {
 
 		System.out.println("Start");
 
-//		String className = "com.mysql.cj.jdbc.Driver";
 		String url = "jdbc:mysql://localhost:3306/deloitte";
 		String user = "root";
 		String password = "root";
 		String sql = "SELECT * FROM student ORDER BY roll_no";
 
-		Connection con = null;
-		Statement st = null;
-		ResultSet rs = null;
-		ResultSetMetaData rsmd = null;
+		try (Connection con = DriverManager.getConnection(url, user, password);
+				Statement st = con.createStatement();
+				ResultSet rs = st.executeQuery(sql);) {
+			ResultSetMetaData rsmd = rs.getMetaData();
 
-//		try {
-//			Class.forName(className);
-//		} catch (ClassNotFoundException e) {
-//			e.printStackTrace();
-//		}
-
-		try {
-			con = DriverManager.getConnection(url, user, password);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		try {
-			st = con.createStatement();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		try {
-			rs = st.executeQuery(sql);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		try {
-			rsmd = rs.getMetaData();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		try {
 			for (int i = 1; i <= rsmd.getColumnCount(); i++) {
 				System.out.print(rsmd.getColumnName(i) + " ");
 			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		System.out.println();
-		try {
+			System.out.println();
 			while (rs.next()) {
 				for (int i = 1; i <= rsmd.getColumnCount(); i++) {
 					System.out.print(rs.getString(i) + "\t");
@@ -75,8 +39,6 @@ public class JdbcDemo {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-
-		System.out.println("End");
 
 	}
 
