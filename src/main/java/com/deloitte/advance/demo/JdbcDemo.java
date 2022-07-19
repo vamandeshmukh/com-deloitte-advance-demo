69246f97d6fad6309a0ca529b66f7fa3b9f8b755
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-07-19 11:21:53 +0530

    coreJava

--- src/main/java/com/deloitte/advance/demo/JdbcDemo.java
+++ src/main/java/com/deloitte/advance/demo/JdbcDemo.java
@@ -1,10 +1,80 @@
 package com.deloitte.advance.demo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class JdbcDemo {
 
 	public static void main(String[] args) {
 
 		System.out.println("Start");
+
+		String className = "com.mysql.cj.jdbc.Driver";
+		String url = "jdbc:mysql://localhost:3306/deloitte";
+		String user = "root";
+		String password = "root";
+		String sql = "SELECT * FROM student ORDER BY roll_no";
+
+		Connection con = null;
+		Statement st = null;
+		ResultSet rs = null;
+		ResultSetMetaData rsmd = null;
+
+		try {
+			Class.forName(className);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			con = DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			st = con.createStatement();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			rs = st.executeQuery(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			rsmd = rs.getMetaData();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+				System.out.print(rsmd.getColumnName(i) + "\t");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println();
+		try {
+			while (rs.next()) {
+				for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+					System.out.print(rs.getString(i) + "\t");
+				}
+				System.out.println();
+			}
+			con.close();
+			st.close();
+			rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
 		System.out.println("End");
 
