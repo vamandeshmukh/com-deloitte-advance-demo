bb248df359eb7a908646a6837d35012bb24db2d2
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-07-19 14:47:08 +0530

    coreJava

--- src/test/java/com/deloitte/advance/demo/CalcTests.java
+++ src/test/java/com/deloitte/advance/demo/CalcTests.java
@@ -3,21 +3,50 @@ package com.deloitte.advance.demo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import com.deloitte.advance.sample.Calc;
 
 public class CalcTests {
 
+	private static Calc cal;
+
+	@BeforeAll
+	public static void setUp() {
+		System.out.println("setUp");
+		cal = new Calc();
+	}
+
+	@AfterAll
+	public static void tearDown() {
+		System.out.println("tearDown");
+		cal = null;
+	}
+
 	@Test
 	public void testAddNums() {
-		Calc cal = new Calc();
+		System.out.println("testAddNums");
 		assertEquals(cal.addNums(10, 20), 30);
 	}
 
 	@Test
 	public void testAddNums2() {
-		Calc cal = new Calc();
+		System.out.println("testAddNums2");
 		assertNotEquals(cal.addNums(10, 20), 31);
 	}
+
+	@Test
+	public void testSubNums() {
+		System.out.println("testSubNums");
+		assertEquals(cal.subNums(20, 10), 10);
+	}
+
+	@Test
+	public void testSubNums2() {
+		System.out.println("testSubNums2");
+		assertNotEquals(cal.subNums(10, 20), 15);
+	}
+
 }
